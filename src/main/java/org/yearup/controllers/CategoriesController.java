8f3073b7abb90c2536c9b3ac1f1a13c7d5a137f2
Romell Walker <walkerromell@gmail.com>
2025-12-15 11:19:08 -0500

    Initial commit

---
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -0,0 +1,80 @@
+package org.yearup.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.yearup.data.CategoryDao;
+import org.yearup.data.ProductDao;
+import org.yearup.models.Category;
+import org.yearup.models.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// add the annotations to make this a REST controller
+// add the annotation to make this controller the endpoint for the following url
+    // http://localhost:8080/categories
+// add annotation to allow cross site origin requests
+@RestController
+//@RequestMapping("categories")
+@CrossOrigin
+public class CategoriesController
+{
+
+    private CategoryDao categoryDao;
+    private ProductDao productDao;
+
+    @Autowired
+    public CategoriesController(CategoryDao categoryDao, ProductDao productDao) {
+        this.categoryDao = categoryDao;
+        this.productDao = productDao;
+    }
+// create an Autowired controller to inject the categoryDao and ProductDao
+
+    // add the appropriate annotation for a get action
+@RequestMapping(path = "categories")
+    public List<Category> getAll()
+    { List<Category> categories = new ArrayList<>();
+        return categories;
+        // find and return all categories
+    }
+
+    // add the appropriate annotation for a get action
+
+    public Category getById(@PathVariable int id)
+    {
+        // get the category by id
+        return null;
+    }
+
+    // the url to return all products in category 1 would look like this
+    // https://localhost:8080/categories/1/products
+    @GetMapping("{categoryId}/products")
+    public List<Product> getProductsById(@PathVariable int categoryId)
+    {
+        // get a list of product by categoryId
+        return null;
+    }
+
+    // add annotation to call this method for a POST action
+    // add annotation to ensure that only an ADMIN can call this function
+    public Category addCategory(@RequestBody Category category)
+    {
+        // insert the category
+        return null;
+    }
+
+    // add annotation to call this method for a PUT (update) action - the url path must include the categoryId
+    // add annotation to ensure that only an ADMIN can call this function
+    public void updateCategory(@PathVariable int id, @RequestBody Category category)
+    {
+        // update the category by id
+    }
+
+
+    // add annotation to call this method for a DELETE action - the url path must include the categoryId
+    // add annotation to ensure that only an ADMIN can call this function
+    public void deleteCategory(@PathVariable int id)
+    {
+        // delete the category by id
+    }
+}
